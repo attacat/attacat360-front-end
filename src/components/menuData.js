ab1238689d67ae22b9d61fdfc3087379b557be00
Sofia Ignatiadi <sphignatiadi@gmail.com>
2022-09-14 15:07:22 +0100

    Create menuData file as well as populate it

---
+++ src/components/menuData.js
@@ -0,0 +1,41 @@
+
+
+const MenuData = [
+  {
+    title: 'Home',
+    path: '',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Guidelines',
+    path: 'instructions',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Set my review date',
+    path: 'review-date',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Select my Reviewers',
+    path: 'select-reviewers',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Reviews to complete',
+    path: 'complete-reviews',
+    cName: 'nav-text'
+  },
+  {
+    title: 'My Feedback',
+    path: 'feedback',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Account',
+    path: 'account',
+    cName: 'nav-text'
+  }
+];
+
+export default MenuData;
