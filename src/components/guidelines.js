79844c230985f775efa9e4de8e9412fd8dcc1bc9
Sofia Ignatiadi <sphignatiadi@gmail.com>
2022-09-14 17:08:48 +0100

    Create and populate guidelines

---
+++ src/components/guidelines.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Guidelines=()=>{
+    return(
+        <div>
+        <h2>Attacat360 guidelines</h2>
+        <p>Welcome to the Attacat 360 process.
+            This happens every March and September and is an opportunity for an open conversation with your line manager around:
+            <li>Feedback you have received from your colleagues</li>
+            <li>Feedback from your line manager</li>
+            <li>Objectives for the coming months</li>
+            <li>Career and personal goals</li>
+            <li>Training requests</li>
+            <li>Issues you want to raise</li>
+            <li>Salary review</li>
+        </p>
+        <h2>Star ratings</h2>
+        <p>
+        Please note the following general guidance around star interpretation:
+        </p>
+        <li>Serious work required</li>
+        <li>Some work required</li>
+        <li>Meets the expected high standard of an Attacat</li>
+        <li>Exceeds the high standard</li>
+        <li>Is an absolute superstar</li>
+        </div>
+    );
+};
+
+export default Guidelines;
